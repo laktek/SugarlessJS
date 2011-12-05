53025945525e8533486262a882a51f0f91e63252
Lakshan Perera <lakshan@web2media.net>
2011-12-06 02:06:10 +0530

    Remove object from stack at the end of lifecycle

--- examples/node/server.js
+++ examples/node/server.js
@@ -59,12 +59,13 @@ http.createServer(function (req, res) {
   // res.writeHead(200, {'Content-Type': 'text/plain'});
   // res.end(message);
 
+  //Sugarless way
   $_(req)(
      getName
    , getLocation
    , prepareMessage
    , sendResponse, "Hello! visitor", res  
-  ) 
+  ); 
    
 }).listen(1337, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:1337/');
